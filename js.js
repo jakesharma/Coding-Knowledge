44cd3c66291e748e1dbd23aea5715e3921d5c646
jakesharma <jaysuthar597@gmail.com>
2024-11-13 17:26:00 +0530

    up

---
+++ js.js
@@ -0,0 +1,12 @@
+
+// function btn_click(nexText){
+//     localStorage.setItem('pagename',nexText);
+// }  
+
+
+// function btn_html(){
+//     var val=localStorage.getItem("pagename");
+//     const x = document.getElementById("changetext").localStorage.getItem("pagename");
+//     x.innerText = Html;
+// }
+
